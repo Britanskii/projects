372b242376fedb40f6ea18f42679e2b4036f880a
Britanskii <ohinkovskii@gmail.com>
2021-03-18 22:44:49 +0300

    Add images, choise lang, mod text, bugfix

--- constructor/src/script/script.js
+++ constructor/src/script/script.js
@@ -1,13 +1,17 @@
 'use strict'
 
-const fonts = document.querySelectorAll('.constructor__choise *'),
+const fonts = document.querySelectorAll('.constructor__cyrillic *, .constructor__latin *'),
     text = document.querySelector('.constructor__text'),
     sizes = document.querySelectorAll('.constructor__size *'),
     colors = document.querySelectorAll('.constructor__select *'),
     select = document.querySelector('.constructor__select'),
     input = document.querySelector('.constructor__input'),
     images = document.querySelectorAll('.constructor__images *'),
-    bg = document.querySelector('.constructor__bg')
+    bg = document.querySelector('.constructor__bg'),
+    lang = document.querySelector('.constructor__fontSelect'),
+    cyrillic = document.querySelector('.constructor__cyrillic'),
+    latin = document.querySelector('.constructor__latin')
+
 
 
 fonts.forEach(font => {
@@ -24,21 +28,28 @@ images.forEach(img => {
 
 sizes.forEach(size => {
     size.addEventListener('click', () => {
-        if (size.innerHTML == 'Мелкий') {
+        if (size.classList.value.includes('small')) {
             text.style.fontSize = '28px';
-        } else if (size.innerHTML == 'Средний') {
+        } else if (size.classList.value.includes('medium')) {
             text.style.fontSize = '36px';
-        } else if (size.innerHTML == 'Большой') {
+        } else if (size.classList.value.includes('big')) {
             text.style.fontSize = '46px';
         }
     });
 });
 
+lang.addEventListener('click', () => {
+    if (!lang.options.selectedIndex) {
+        latin.style.display = 'none'
+        cyrillic.style.display = 'flex'
+    } else {
+        cyrillic.style.display = 'none'
+        latin.style.display = 'flex'
+    }
+});
 
 select.addEventListener('click', () => {
-    console.log(colors[select.options.selectedIndex].value);
-    text.style.color = colors[select.options.selectedIndex].value;
-    text.style.textShadow = `0 0 30px ${colors[select.options.selectedIndex].value}`;
+    text.style.textShadow = `0 0 30px ${colors[select.options.selectedIndex].value}, 0 0 30px ${colors[select.options.selectedIndex].value}`;
 });
 
 input.addEventListener('keyup', () => {
